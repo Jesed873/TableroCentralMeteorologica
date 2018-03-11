4d899ade5e73b0d6c22897f3d484a2473823f4ec
EstebanCureno <estebancureno@hotmail.com>
2018-03-11 03:32:28 -0600

    este es otro ejemplo

--- phpinfo.php
+++ phpinfo.php
@@ -1,5 +1,6 @@
 <?php 
     echo "Este es un archivo PHP";
     echo "Esta es la modificacion en el branch modificacion";
+    echo "http://127.0.0.1/TableroCentralMeteorologica/phpinfo.php";
     phpinfo();
 ?>
